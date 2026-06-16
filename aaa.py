fb0bb895800bb059819ab2e8bc26a0f22be9c863
Ibrokhimjonovv <megabytekekabyte@gmail.com>
2026-06-16 14:10:50 +0300

    .

--- aaa.py
+++
@@ -1,8 +0,0 @@
-son = int(input("Son kiriting: "))
-
-if son > 0:
-    print("Kiritilgan son musbat")
-elif son == 0:
-    print("Kiritlgan son 0 ga teng")
-else:
-    print("Kiritilgan son manfiy")
